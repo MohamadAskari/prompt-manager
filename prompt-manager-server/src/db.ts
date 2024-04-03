734b7e620544409a36502006ce51aa6b2e573f6f
Mohamad Askari <askari.2105681@studenti.uniroma1.it>
2024-04-03 14:59:59 +0200

    Installed ts-node globally

--- prompt-manager-server/src/db.ts
+++ prompt-manager-server/src/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'redis';
 
-export const client = createClient({url: "redis://redis:6379"});
+export const client = createClient({url: "redis://127.0.0.1:6379"});
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.connect()
