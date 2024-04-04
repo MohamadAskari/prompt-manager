8844396380bbade0852e21ef56ce84f75a81b0b1
Mohamad Askari <askari.2105681@studenti.uniroma1.it>
2024-04-04 16:32:01 +0200

    Added test file

--- prompt-manager-server/src/db.ts
+++ prompt-manager-server/src/db.ts
@@ -1,5 +1,13 @@
 import { createClient } from 'redis';
 
-export const client = createClient({url: "redis://redis:6379"});
+let redisUrl: string;
+
+if (process.env.RUNNING_IN_DOCKER === 'true') {
+    redisUrl = 'redis://redis:6379';
+} else {
+    redisUrl = 'redis://127.0.0.1:6379';
+}
+
+export const client = createClient({url: redisUrl});
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.connect()
